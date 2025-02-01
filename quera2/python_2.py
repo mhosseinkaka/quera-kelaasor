2f36a0ba8cea61957a79dc14430630178836a5dd
mr.kaka <est4.mh.kaka@gmail.com>
2025-02-01 15:17:56 +0330

    v02

--- quera2/python_2.py
+++ quera2/python_2.py
@@ -1,9 +1,11 @@
 n = int(input())
-m = ""
-for i in range(n + 1):
-    v = 10 ** i
-    m += str(v)
+m = "".join(str(10**i) for i in range(n+1))
 print(m[n - 1])
+
+# for i in range(n + 1):
+#     v = 10 ** i
+#     m += str(v)
+
 
 
 
