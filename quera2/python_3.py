34433b4307540ae146d47503286629e706fe0d16
mr.kaka <est4.mh.kaka@gmail.com>
2025-02-01 17:17:51 +0330

    v01

---
+++ quera2/python_3.py
@@ -0,0 +1,14 @@
+import string
+l1 = list(string.ascii_lowercase)
+l2 = list(string.ascii_uppercase)
+n = int(input())
+m = input()
+i = 0
+while i < len(m):
+    for value in 
+
+
+
+
+print(l1)
+print(l2)
