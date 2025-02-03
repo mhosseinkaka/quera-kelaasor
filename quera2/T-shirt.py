17195a99b3a12e7e9bc6511547df632adb2a85f4
mhosseinkaka <mhosseinkaka@gmail.com>
2025-02-04 01:08:29 +0330

    with bug

--- quera2/T-shirt.py
+++ quera2/T-shirt.py
@@ -1,14 +1,42 @@
+def kelaasor_shirt(choice, list_shirt):
+    for i in range(len(choice)):
+        
+        key = choice[i]
+        if list_shirt[key] > 0:
+            list_shirt[key] -= 1
+            print(list_shirt[key]) 
+        else:
+            if  list_shirt[choice[i + 1]] > 0:
+                list_shirt[choice[i + 1]] -= 1
+                print(list_shirt[choice[i + 1]])
+            else:
+                if list_shirt[choice[i - 1]] > 0:
+                    list_shirt[choice[i - 1]] -= 1
+                    print(list_shirt[choice[i - 1]])
+                else:
+                    if list_shirt[choice[i + 2]] > 0:
+                        list_shirt[choice[i + 2]] -= 1
+                        print(list_shirt[choice[i + 2]])
+                    else:
+                        if list_shirt[choice[i - 2]] > 0:
+                            list_shirt[choice[i - 2]] -= 1
+                            print(list_shirt[choice[i - 2]])
+                        else:
+                            list_shirt[key] = "No-Shirt"
+
+
 number_shirt = list(map(int, input().split()))
 size = ["S", "M", "L", "XL", "XXL"]
 list_shirt = dict(zip(size, number_shirt))
-print(list_shirt)
+# print(list_shirt)
 person = int(input())
-print(person)
+# print(person)
 choice = []
 for i in range(person):
     input_size = input().upper()
     choice.append(input_size)
-print(choice)
+# print(choice)
 
-def assign(choice, list_shirt):
-    
+result = kelaasor_shirt(choice, list_shirt)
+for key, value in result.items():
+    print(value)
