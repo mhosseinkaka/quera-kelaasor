b3efb69e9eb6a04adbce1447ed4ecb46fe2ce264
mr.kaka <est4.mh.kaka@gmail.com>
2025-02-17 15:28:25 +0330

    V02

--- quera4/Be_a_human.py
+++ quera4/Be_a_human.py
@@ -1,21 +1,23 @@
-'''
-
-This class takes a name and an age,
-calls the birthday method and and returns the output as a number.
-
-'''
 
 class Person:
+    '''
+    This class takes a name and an age,
+    calls the birthday method and and returns the output as a number.
+    '''
     def __init__(self, name: str, age: int):
+        '''
+        Creates an object by taking a name and age.
+        '''
         assert type(name) == str, "enetr yor currect name"
         assert type(age) == int, "enter your age as a number"
         self.name = name
         self.age = age
 
-    """
-    The birthday method takes a number as input, adds one to it, and returns the new number.
-    """
+    
     def birthday(self) -> int: 
+        '''
+        The birthday method takes a number as input, adds one to it, and returns the new number.
+        '''
         self.age += 1
         return self.age
 
